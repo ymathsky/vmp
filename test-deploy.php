dc0a35598702cea8f4dd1e38bccffcc64704e029
VMP <care@visitingmedicalphysician.com>
2026-03-18 01:41:59 +0800

    Fix .cpanel.yml and add deployment test page

---
+++ test-deploy.php
@@ -0,0 +1,3 @@
+<?php
+echo '<h1 style="font-family:sans-serif;color:green;">&#10003; Deployment is working!</h1>';
+echo '<p style="font-family:sans-serif;">Deployed on: <strong>' . date('Y-m-d H:i:s') . '</strong></p>';
