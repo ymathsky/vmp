7bba07c4971b422e978c0b6146eb4a84b4e2fd47
VMP <care@visitingmedicalphysician.com>
2026-03-18 01:57:22 +0800

    Remove test deployment file

--- test-deploy.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '<h1 style="font-family:sans-serif;color:green;">&#10003; Deployment is working!</h1>';
-echo '<p style="font-family:sans-serif;">Deployed on: <strong>' . date('Y-m-d H:i:s') . '</strong></p>';
